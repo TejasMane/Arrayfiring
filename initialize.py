aff69d36633cf506987cc3ed700bc6bb62b17824
TejasMane <ezio.tejas@gmail.com>
2017-02-14 10:57:41 +0530

    Add files via upload

--- initialize.py
+++ initialize.py
@@ -3,16 +3,17 @@ from scipy.special import erfinv
 import h5py
 import params
 
-"""Here we shall assign values as set in params"""
+"""Here we shall re-assign values as set in params"""
 
 no_of_particles      = params.no_of_particles
-simulation_dimension = params.simulation_dimension
-restart_simulation   = params.restart_simulation
 choice_integrator    = params.choice_integrator
 collision_operator   = params.collision_operator
 
-if(collision_operator == "hardsphere"):
-  scattering_distance = params.scattering_distance
+plot_spatial_temperature_profile = params.plot_spatial_temperature_profile
+
+if(plot_spatial_temperature_profile == "true"):
+  x_zones_temperature = params.x_zones_temperature
+  y_zones_temperature = params.y_zones_temperature
 
 elif(collision_operator == "potential-based"):
   potential_steepness     = params.potential_steepness
@@ -20,10 +21,9 @@ elif(collision_operator == "potential-based"):
   order_finite_difference = params.order_finite_difference
 
 elif(collision_operator == "montecarlo"):
-  x_zones            = params.x_zones
-  y_zones            = params.y_zones
-  scattered_fraction = params.scattered_fraction
-
+  x_zones_montecarlo = params.x_zones_montecarlo
+  y_zones_montecarlo = params.y_zones_montecarlo
+  
 mass_particle      = params.mass_particle
 boltzmann_constant = params.boltzmann_constant
 T_initial          = params.T_initial
@@ -43,6 +43,16 @@ if(wall_condition_z == "thermal"):
   T_front_wall = params.T_front_wall
   T_back_wall  = params.T_back_wall
 
+fields_enabled   = params.fields_enabled
+
+if(fields_enabled == "true"):
+  spread            = params.spread
+  ghost_cells       = params.ghost_cells
+  speed_of_light    = params.speed_of_light
+  charge            = params.charge
+  x_zones_field     = params.x_zones_field
+  y_zones_field     = params.y_zones_field
+
 left_boundary    = params.left_boundary
 right_boundary   = params.right_boundary
 length_box_x     = params.length_box_x
@@ -55,13 +65,11 @@ back_boundary    = params.back_boundary
 front_boundary   = params.front_boundary
 length_box_z     = params.length_box_z
 
-#Here we complete import of all the variable from the parameters file
-
 """ 
 This file is used in providing the initial velocities and positions to the particles.
 The distribution of your choice may be obtained by modifying the options that have been 
 provided below. Although the choice of function must be changed from this file, the parameter 
-change may also be made at params.py
+change must be made at params.py so that the same values are reflected across all the modules
 """
 
 """ Initializing the positions for the particles """
@@ -78,35 +86,53 @@ R2 = np.random.rand(no_of_particles)
 R3 = np.random.rand(no_of_particles)
 
 # Sampling velocities corresponding to Maxwell-Boltzmann distribution at T_initial
-initial_velocity_x = np.sqrt(2*boltzmann_constant*T_initial/mass_particle)*erfinv(2*R1-1)
-initial_velocity_y = np.sqrt(2*boltzmann_constant*T_initial/mass_particle)*erfinv(2*R2-1)
-initial_velocity_z = np.sqrt(2*boltzmann_constant*T_initial/mass_particle)*erfinv(2*R3-1)
+initial_vel_x = np.sqrt(2*boltzmann_constant*T_initial/mass_particle)*erfinv(2*R1-1)
+initial_vel_y = np.sqrt(2*boltzmann_constant*T_initial/mass_particle)*erfinv(2*R2-1)
+initial_vel_z = np.sqrt(2*boltzmann_constant*T_initial/mass_particle)*erfinv(2*R3-1)
 
 """ Time parameters for the simulation """
 
-box_crossing_time_scale = (length_box_x/np.max(initial_velocity_x))
-final_time              = 5.0  #20 * box_crossing_time_scale
+# Any time parameter changes that need to be made for the simulation should be edited here:
+box_crossing_time_scale = (length_box_x/np.max(initial_vel_x))
+final_time              = 20 * box_crossing_time_scale
 dt                      = 0.001 * box_crossing_time_scale
 time                    = np.arange(0, final_time, dt)
 
-""" Writing the data to a file """
+""" Parameters for fields """
 
-if(simulation_dimension == 3):
-  h5f = h5py.File('data_files/initial_conditions/initial_data.h5', 'w')
-  h5f.create_dataset('time',          data = time)
-  h5f.create_dataset('x_coordinates', data = initial_position_x)
-  h5f.create_dataset('y_coordinates', data = initial_position_y)
-  h5f.create_dataset('z_coordinates', data = initial_position_z)
-  h5f.create_dataset('velocity_x',    data = initial_velocity_x)
-  h5f.create_dataset('velocity_y',    data = initial_velocity_y)
-  h5f.create_dataset('velocity_z',    data = initial_velocity_z)
-  h5f.close()
+# The following lines define the staggered set of points which shall be used in solving of electric and magnetic fields
 
-if(simulation_dimension == 2):
-  h5f = h5py.File('data_files/initial_conditions/initial_data.h5', 'w')
-  h5f.create_dataset('time',          data = time)
-  h5f.create_dataset('x_coordinates', data = initial_position_x)
-  h5f.create_dataset('y_coordinates', data = initial_position_y)
-  h5f.create_dataset('velocity_x',    data = initial_velocity_x)
-  h5f.create_dataset('velocity_y',    data = initial_velocity_y)
-  h5f.close()
+if(fields_enabled == "true"):
+  dx       = length_box_x/x_zones_field
+  dy       = length_box_y/y_zones_field
+
+  x_center = np.linspace(-ghost_cells*dx, length_box_x + ghost_cells*dx, x_zones_field + 1 + 2*ghost_cells)
+  y_center = np.linspace(-ghost_cells*dy, length_box_y + ghost_cells*dy, y_zones_field + 1 + 2*ghost_cells)
+
+  """ Setting the offset spatial grids """
+
+  x_right = np.linspace(-ghost_cells*(dx/2), length_box_x + (2*ghost_cells + 1)*(dx/2), x_zones_field + 1 + 2*ghost_cells)
+  y_top   = np.linspace(-ghost_cells*(dy/2), length_box_y + (2*ghost_cells + 1)*(dy/2), y_zones_field + 1 + 2*ghost_cells)
+
+  final_time = 2
+  dt         = np.float(dx / (2 * speed_of_light))
+  time       = np.arange(0, final_time, dt)
+
+""" Writing the data to a file which can be accessed by a solver"""
+
+h5f = h5py.File('data_files/initial_conditions/initial_data.h5', 'w')
+h5f.create_dataset('time',          data = time)
+h5f.create_dataset('x_coords', data = initial_position_x)
+h5f.create_dataset('y_coords', data = initial_position_y)
+h5f.create_dataset('z_coords', data = initial_position_z)
+h5f.create_dataset('vel_x',    data = initial_vel_x)
+h5f.create_dataset('vel_y',    data = initial_vel_y)
+h5f.create_dataset('vel_z',    data = initial_vel_z)
+
+if(fields_enabled == "true"):
+  h5f.create_dataset('x_center',      data = x_center)
+  h5f.create_dataset('y_center',      data = y_center)
+  h5f.create_dataset('x_right',       data = x_right)
+  h5f.create_dataset('y_top',         data = y_top)
+
+h5f.close()
--- params.py
+++ params.py
@@ -1,69 +1,76 @@
-no_of_particles      = 100000
+no_of_particles      = 1
 mass_particle        = 1.0
 boltzmann_constant   = 1.0
-# Change this value to 2 or 3 depending upon whether simulation is in 2D or 3D
-simulation_dimension = 2 
-
-# Note that simulation time parameters must be changed from the initialize file
-
-"""Options in development"""
-
-restart_simulation   = "false"
-if(restart_simulation  == "true"):
-  restart_time_index = 100
-  
-
-"""End of options in current developement"""
 
 # If the user requests the written data files will contain the spacial temperature array of each time-step
-
 plot_spatial_temperature_profile = "false"
-# Currently the spatial temperature profile is limited to 2D cases
 
 if(plot_spatial_temperature_profile == "true"):
-  x_zones = 100
-  y_zones = 100
+  x_zones_temperature = 100
+  y_zones_temperature = 100
 
 """Defining the choice for the integrator"""
-# The following options are available for integrators:
+# The following options are currently available for integrators:
 # Option "verlet"    - Velocity Verlet algorithm
-# Option "4th-order" - 4th order symplectic method
-# Option "6th-order" - 6th order symplectic method(to be added)
-# Option "8th-order" - 8th order symplectic method(to be added)
 
 choice_integrator = "verlet"
 
 """ Defining the collision parameters """
 # The following collision kernels are available for implementation
 # Option "collisionless"   - Collisionless Model
-# Option "hardsphere"      - HS Model
 # Option "potential-based" - Potential Based Scattering
 # Option "montecarlo"      - MonteCarlo Scattering
 
-collision_operator = "montecarlo"
+collision_operator = "collisionless"
 
-# scattering_distance is the distance between 2 particles below which scattering kernel is activated
-if(collision_operator == "hardsphere"):
-  scattering_distance = 0.01
-
-# unlike HS model, the minimum scattering distance can only be altered by shifting the potential function found in collision_operators/potential.py
-# default function for potential = potential_magnitude * (-tanh(potential_gradient*distance) + 1)
-# here you can change the potential gradient and potential magnitude
-elif(collision_operator == "potential-based"):
+# The minimum scattering distance can only be altered by shifting the potential function found in collision_operators/potential.py
+# Default function for potential = potential_magnitude * (-tanh(potential_gradient*distance) + 1)
+# Here you can change the potential gradient and potential magnitude, and the order used for finite differencing
+if(collision_operator == "potential-based"):
   potential_steepness     = 300
   potential_amplitude     = 20
   order_finite_difference = 4
 
 # in Monte-Carlo scattering, particles are scattered depending upon the local temperature of the zone they prevail in
-# this eliminates the distance check between particles
+# this eliminates the distance check between particles.
+# Although a selected fraction of the particles can be scattered. This hasn't been implemented yet
+# Currently all particles in the zone are scattered
 elif(collision_operator == "montecarlo"):
-  x_zones            = 50
-  y_zones            = 50
-  scattered_fraction = 1.0 # Yet to be implemented. All particles are being scattered currently   
+  x_zones_montecarlo = 50
+  y_zones_montecarlo = 50
+
+""" Definining parameters for electric and magnetic fields """
+
+fields_enabled   = "true"
+
+if(fields_enabled == "true"):
+  spread          = 0.1     # Shall be used to assign Gaussian
+  ghost_cells     = 1       # Refers to the number of cells beyond the physical domain(usually set to 1)
+  speed_of_light  = 1
+  charge          = 1 # 1e-10   # Charge of each individual particle in the simulation
+  x_zones_field   = 100     # Refers to the number of x-divisions for the cells that are used to compute fields, and currents
+  y_zones_field   = 100     # Refers to the number of y-divisions for the cells that are used to compute fields, and currents
+  forward_row     = [[1], [-1], [0]]
+  forward_column  = [[1, -1, 0]]
+  backward_row    = [[0], [1], [-1]]
+  backward_column = [[0, 1, -1]]
+
+"""
+[[1, -1, 0]] = data[col +1] - data[col]
+
+[[1], [-1], [0]] = data[row]=  data[row + 1] - data[row]
+
+[[0], [1], [-1]] = data[row] = data[row] - data[row-1]
+
+[[0, 1, -1]] = data[col] = data[col] - data[col-1]
+
+"""
+
+
 
 """ Wall and temperature parameters """
 
-# we shall define the different wall conditions that may be implemented:
+# We shall define the different wall conditions that may be implemented:
 # Option "periodic" - Periodic B.C's at the walls
 # Option "hardwall" - Hardwall B.C's at the walls
 # Option "thermal"  - Thermal B.C's at the walls
